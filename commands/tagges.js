f5984e1a20c9074757f4ca5edae7dc03b9a9c3ba
Glitch (hello-express) <none>
2021-06-18 19:01:35 +0000

    🚇🛰 Checkpoint ./commands/tagges.js:32888473/52

--- commands/tagges.js
+++ commands/tagges.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js")
+const Strom = require("discord.js")
 const ayarlar = require("../ayarlar.json")
 
 module.exports.run = async (client, message, args) => {
-    if(!ayarlar.yetkiliRol.some(mattheee => message.member.roles.cache.has(mattheee)) && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Bu işlemi gerçekleştirmek için gerekli yetkin yok!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
+    if(!ayarlar.yetkiliRol.some(Strom => message.member.roles.cache.has(Strom)) && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Bu işlemi gerçekleştirmek için gerekli yetkin yok!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
 
     const etiketlenenKişi = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 if(!etiketlenenKişi) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Tag rolü vermek için bir kişi etiketlemelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
 
-const MessageEmbed = new Discord.MessageEmbed()
+const MessageEmbed = new Strom.MessageEmbed()
 .setColor("RANDOM")
 .setFooter(ayarlar.footer)
 .setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true}))
-.setTimestamp()
+.setTimestamp
 
 if(etiketlenenKişi.roles.cache.has(ayarlar.tagRol)) return message.channel.send(MessageEmbed.setDescription(`Kullanıcıdan başarıyla taglı <@&${ayarlar.tagRol}> rolü alındı!`)).then(etiketlenenKişi.roles.remove(ayarlar.tagRol))
 
