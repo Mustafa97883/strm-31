bd755fb8b7aa79ffa442f6ce5ab8dfa0fceacadb
Glitch (ilhanilhan2312) <none>
2022-08-26 13:46:14 +0000

    👰🌼 Checkpoint ./komutlar/rütbe-top.js:29720564/4078 ./komutlar/rütbe-davetkanal-sıfırla.js:29720564/4095 ./Helpers/Database.js:29720564/2517

--- komutlar/rütbe-top.js
+++ komutlar/rütbe-top.js
@@ -1,30 +1,25 @@
 const Discord = require("discord.js");
 const Database = require("../Helpers/Database");
-// exports.onLoad = (client) => {};
-/**
- * @param {Discord.Client} client 
- * @param {Discord.Message} message 
- * @param {Array<String>} args 
- */
-exports.run = async (client, message, args) => {
-    const db = new Database("./Servers/" + message.guild.id, "Invites");
-    var data = db.get(`invites`) || {};
 
-    var list = Object.keys(data).map(_data => {
-        return {
-            Id: _data,
-            Value: (data[_data].total || 0) + (data[_data].bonus || 0)
-        };
-    }).sort((x, y) => y.Value - x.Value);
+exports.run = async (client, message, args) => {
+    const db = new Database("./Servers/" + message.guild.id, "Rewards");
+    var data = db.get(`rewards`) || {};
+
+    var list = data.sort((x, y) => y.targetInvite - x.targetInvite);
+      if(list.length === 0){
+        var yok = new Discord.MessageEmbed()
+            .setAuthor("Hiç Bir Rank Ayarlanmamış")
+            .setDescription("Ayarlamak İçin s!rütbe-ekle @rol davetsayi");
+            return message.channel.send(yok);
+        }
 
     var embed = new Discord.MessageEmbed()
-        .addField("Davetler", `
-    ** **${list.splice(0, 10).map((item, index) => `\`${index + 1}.\` <@${item.Id}>: \`${item.Value} Davet\``).join("\n")}
+        .addField("Rütbeler", `
+    ** **${list.splice(0, 10).map((item, index) => `\`${index + 1}.\` <@&${item.Id}>: \`${item.Invite} Davete\``).join("\n")}
     `);
 
     message.channel.send(embed);
 };
-
 exports.conf = {
   enabled: true,
   guildOnly: false,
@@ -32,7 +27,7 @@ exports.conf = {
   permLevel: 0
 };
 exports.help = {
-  name: 'top',
+  name: 'rütbeler',
   description: 'Logo Yaparsınız',
   usage: 'm-logo <yazı>'
 };
