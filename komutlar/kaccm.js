146e6247f4d917bc9b53fbd3ed05c8abd3e341f0
Glitch (ilhanilhan2312) <none>
2022-08-26 14:10:12 +0000

    💵👩 Checkpoint ./komutlar/kaccmm.js:29720564/102 ./komutlar/kaccm.js:29720564/208

--- komutlar/kaccm.js
+++ komutlar/kaccm.js
@@ -7,7 +7,7 @@ exports.run = (client, message) => {
   
   
    message.channel.send('Hemen Diyorum Abi 1 Saniye..').then(message => {
-   var espriler = [' **Senin Malafatın  18CM ** :eggplant: ' ,'**Senin Malafatın  11CM ** :eggplant:' ,'**Senin Malafatın 31CM sjsj  ** :eggplant:' ,'**Senin Malafatın  35CM ** :eggplant:' ,'**Senin Malafatın  8CM  ** :eggplant:' ,'**Senin Malafatın  65CM  ** :eggplant:' ,'**Senin Malafatın 5CM  ** :eggplant:' ,'**Senin Malafatın 31CM  ** :eggplant:' ,'**Senin Malafatın  14CM ** :eggplant:' ,'**Senin Malafatın  1CM ** :eggplant:'];
+   var espriler = [' **Senin Malafatın  18CM ** :eggplant: ' ,'**Senin Malafatın  11CM ** :eggplant:' ,'**Senin Malafatın 31CM sjsj  ** :eggplant:' ,'**Senin Malafatın  35CM ** :eggplant:' ,'**Senin Malafatın  8CM  ** :eggplant:' ,'**Senin Malafatın  65CM oyyyy  ** :eggplant:' ,'**Senin Malafatın 5CM  ** :eggplant:' ,'**Senin Malafatın  14CM ** :eggplant:' ,'**Senin Malafatın  1CM Bune amk ** :eggplant:'];
       var espri = espriler[Math.floor(Math.random() * espriler.length)];
             message.edit(`${espri}`);
  });
